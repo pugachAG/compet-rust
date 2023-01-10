c5a56002cbc1779b7f9b06957634e63fa5fd6def
Anton Puhach <pugachag@gmail.com>
2023-01-10 20:14:56 +0100

    Rename as_vec -> into_vec

--- src/utils/collections.rs
+++ src/utils/collections.rs
@@ -1,15 +1,15 @@
 pub fn def_vec<T: Default>(n: usize) -> Vec<T> {
-    (0..n).map(|_| T::default()).collect()
+    (0..n).map(|_| T::default()).into_vec()
 }
 
-pub trait AsVecExt {
+pub trait IntoVecExt {
     type Item;
-    fn as_vec(self) -> Vec<Self::Item>;
+    fn into_vec(self) -> Vec<Self::Item>;
 }
 
-impl<T: Iterator> AsVecExt for T {
+impl<T: Iterator> IntoVecExt for T {
     type Item = T::Item;
-    fn as_vec(self) -> Vec<Self::Item> {
+    fn into_vec(self) -> Vec<Self::Item> {
         self.collect::<Vec<_>>()
     }
 }
