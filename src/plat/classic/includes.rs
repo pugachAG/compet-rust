44e40e05ae147aa49e9d4998fda4f8ad9737cdbb
Anton Puhach <pugachag@gmail.com>
2023-01-10 10:20:28 +0100

    Introduce iterator .as_vec() = .collect::<Vec<_>>()

--- src/plat/classic/includes.rs
+++ src/plat/classic/includes.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 pub use super::io::Io;
+pub use crate::utils::collections::{def_vec, AsVecExt};
 pub use crate::{input, output};
-pub use crate::utils::collections::def_vec;
-pub use std::collections::{HashMap, HashSet, BinaryHeap, BTreeSet, BTreeMap, VecDeque};
-pub use std::cmp::{min, max, Reverse};
+pub use std::cmp::{max, min, Reverse};
+pub use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
--- src/utils/collections.rs
+++ src/utils/collections.rs
@@ -1,3 +1,15 @@
 pub fn def_vec<T: Default>(n: usize) -> Vec<T> {
     (0..n).map(|_| T::default()).collect()
 }
+
+pub trait AsVecExt {
+    type Item;
+    fn as_vec(self) -> Vec<Self::Item>;
+}
+
+impl<T: Iterator> AsVecExt for T {
+    type Item = T::Item;
+    fn as_vec(self) -> Vec<Self::Item> {
+        self.collect::<Vec<_>>()
+    }
+}
