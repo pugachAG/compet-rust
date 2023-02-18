7f9c66532a84b6fe2eac20cf5a6bab9a0d7c78c9
Anton Puhach <pugachag@gmail.com>
2023-02-18 21:23:42 +0100

    Change bin search API

--- src/algo/bin_search.rs
+++ src/algo/bin_search.rs
@@ -17,33 +17,11 @@ pub fn bin_search_range<T: Ord, B: RangeBounds<T>>(a: &[T], range: B) -> Range<u
     n..n
 }
 
-fn bin_search_end<T: Ord>(a: &[T], b: Bound<&T>) -> Option<usize> {
-    let n = a.len();
-    if matches!(b, Bound::Unbounded) && !a.is_empty() {
-        return Some(n - 1);
-    }
-    let mut bs = BinSearch::new(0..n);
-    while let Some(token) = bs.next() {
-        let cur = &a[token.val];
-        let ok = match b {
-            Bound::Included(v) => cur <= v,
-            Bound::Excluded(v) => cur < v,
-            Bound::Unbounded => unreachable!(),
-        };
-        if ok {
-            token.update_left(true);
-        } else {
-            token.update_right(false);
-        }
-    }
-    bs.answer()
-}
-
 fn bin_search_start<T: Ord>(a: &[T], b: Bound<&T>) -> Option<usize> {
     if matches!(b, Bound::Unbounded) && !a.is_empty() {
         return Some(0);
     }
-    let mut bs = BinSearch::new(0..a.len());
+    let mut bs = BinSearch::suffix(0..a.len());
     while let Some(token) = bs.next() {
         let cur = &a[token.val];
         let ok = match b {
@@ -51,11 +29,25 @@ fn bin_search_start<T: Ord>(a: &[T], b: Bound<&T>) -> Option<usize> {
             Bound::Excluded(v) => cur > v,
             Bound::Unbounded => unreachable!(),
         };
-        if ok {
-            token.update_right(true);
-        } else {
-            token.update_left(false);
-        }
+        token.update(ok);
+    }
+    bs.answer()
+}
+
+fn bin_search_end<T: Ord>(a: &[T], b: Bound<&T>) -> Option<usize> {
+    let n = a.len();
+    if matches!(b, Bound::Unbounded) && !a.is_empty() {
+        return Some(n - 1);
+    }
+    let mut bs = BinSearch::prefix(0..n);
+    while let Some(token) = bs.next() {
+        let cur = &a[token.val];
+        let ok = match b {
+            Bound::Included(v) => cur <= v,
+            Bound::Excluded(v) => cur < v,
+            Bound::Unbounded => unreachable!(),
+        };
+        token.update(ok);
     }
     bs.answer()
 }
@@ -80,12 +72,28 @@ impl<T: Integer> Boundary<T> {
     }
 }
 
-/// # Examples
+#[derive(Copy, Clone)]
+pub enum BinSearchLayout {
+    Prefix,
+    Suffix,
+}
+
+/// # Example
 /// ```
+/// fn isqrt(x: u32) -> u32 {
+///     let mut bs = BinSearch::prefix(0..=x);
+///     while let Some(token) = bs.next() {
+///         let ok = token.val().pow(2) <= x;
+///         token.update(ok);
+///     }
+///     bs.answer().unwrap()
+/// }
+/// assert_eq!(isqrt(30), 5);
 /// ```
 pub struct BinSearch<T: Integer> {
     left: Boundary<T>,
     right: Boundary<T>,
+    layout: BinSearchLayout,
     ans: Option<Option<T>>,
 }
 
@@ -99,17 +107,25 @@ impl<T: Integer> BinSearchDecisionToken<'_, T> {
         self.val
     }
 
-    pub fn update_left(self, ok: bool) {
-        self.bs.set_left(self.val, ok);
-    }
-
-    pub fn update_right(self, ok: bool) {
-        self.bs.set_right(self.val, ok);
+    pub fn update(self, ok: bool) {
+        self.bs.update(self.val, ok);
     }
 }
 
 impl<T: Integer> BinSearch<T> {
-    pub fn new(range: impl RangeBounds<T>) -> Self {
+    /// [1 1 1 1 0 0]
+    ///        ^
+    pub fn prefix(range: impl RangeBounds<T>) -> Self {
+        Self::new(range, BinSearchLayout::Prefix)
+    }
+
+    /// [0 0 0 1 1 1 1]
+    ///        ^
+    pub fn suffix(range: impl RangeBounds<T>) -> Self {
+        Self::new(range, BinSearchLayout::Suffix)
+    }
+
+    pub fn new(range: impl RangeBounds<T>, layout: BinSearchLayout) -> Self {
         let val_left_incl = match range.start_bound() {
             Bound::Included(val) => *val,
             Bound::Excluded(val) => *val + 1.into(),
@@ -125,12 +141,14 @@ impl<T: Integer> BinSearch<T> {
                 left: Boundary::unconfirmed(val_left_incl),
                 // keep left exclusive to avoid underflow when val_right_excl == 0usize
                 right: Boundary::unconfirmed(val_right_excl),
+                layout,
                 ans: Some(None),
             }
         } else {
             Self {
                 left: Boundary::unconfirmed(val_left_incl),
                 right: Boundary::unconfirmed(val_right_excl - 1.into()),
+                layout,
                 ans: None,
             }
         }
@@ -152,6 +170,17 @@ impl<T: Integer> BinSearch<T> {
             v
         } else {
             panic!("binary search is not done")
+        }
+    }
+
+    fn update(&mut self, val: T, ok: bool) {
+        match (self.layout, ok) {
+            (BinSearchLayout::Prefix, true) | (BinSearchLayout::Suffix, false) => {
+                self.set_left(val, ok)
+            }
+            (BinSearchLayout::Prefix, false) | (BinSearchLayout::Suffix, true) => {
+                self.set_right(val, ok)
+            }
         }
     }
 
