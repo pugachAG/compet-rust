6df94dd3f10bce46745a044aae84fb54fe285a2b
Anton Puhach <pugachag@gmail.com>
2023-11-09 19:50:53 +0100

    Updage segment tree interface

--- src/ds/seg_tree.rs
+++ src/ds/seg_tree.rs
@@ -1,14 +1,123 @@
 use std::ops::{Range, RangeBounds};
 
-use crate::plat::classic::includes::def_vec;
 use crate::utils::misc::{assert_range, unpack_range};
 
-type CombineFn<T> = fn(&T, &T) -> T;
+/// # Example
+/// ```
+/// #[derive(Copy, Clone)]
+/// struct SegTreeSum(i32);
+///
+/// impl SegTreeMonoid for SegTreeSum {
+///     fn op(l: Self, r: Self) -> Self {
+///         Self(l.0 + r.0)
+///     }
+///     
+///     fn e() -> Self {
+///         Self(0)
+///     }
+/// }
+/// ```
+pub trait SegTreeMonoid: Copy {
+    fn op(l: Self, r: Self) -> Self;
+
+    /// Identity element: op(x, e()) = op(e(), x) = x
+    fn e() -> Self;
+}
 
 pub struct SegTree<T> {
     n: usize,
     st: Vec<T>,
-    f: CombineFn<T>,
+}
+
+impl<T: SegTreeMonoid> SegTree<T> {
+    pub fn with_size(n: usize) -> Self {
+        Self {
+            n,
+            st: vec![T::e(); Self::st_len(n)],
+        }
+    }
+
+    pub fn new(a: &[T]) -> Self {
+        let n = a.len();
+        let mut ret = Self::with_size(n);
+        if n > 0 {
+            ret.build(a, &ret.root());
+        }
+        ret
+    }
+
+    pub fn get(&self, rng_bounds: impl RangeBounds<usize>) -> T {
+        let rng = unpack_range(rng_bounds);
+        assert_range(&rng, 0..self.n, true);
+        if rng.is_empty() {
+            T::e()
+        } else {
+            self.calc(&rng, &self.root())
+        }
+    }
+
+    pub fn set(&mut self, i: usize, val: T) {
+        assert!(i < self.n);
+        self.update(i, val, &self.root());
+    }
+
+    fn update(&mut self, i: usize, val: T, pos: &Pos) {
+        if pos.single_point().is_some() {
+            self.st[pos.st_i] = val;
+        } else {
+            let (ref left, ref right) = pos.split();
+            if left.contains(i) {
+                self.update(i, val, left);
+            } else {
+                self.update(i, val, right);
+            }
+            self.recalc(pos, left, right);
+        }
+    }
+
+    fn calc(&self, range: &Range<usize>, pos: &Pos) -> T {
+        if pos.is_inside(range) {
+            return self.st[pos.st_i];
+        }
+        let (ref left, ref right) = pos.split();
+        if !right.intersects(range) {
+            self.calc(range, left)
+        } else if !left.intersects(range) {
+            self.calc(range, right)
+        } else {
+            T::op(self.calc(range, left), self.calc(range, right))
+        }
+    }
+
+    fn build(&mut self, a: &[T], pos: &Pos) {
+        if let Some(i) = pos.single_point() {
+            self.st[pos.st_i] = a[i];
+        } else {
+            let (ref left, ref right) = pos.split();
+            self.build(a, left);
+            self.build(a, right);
+            self.recalc(pos, left, right);
+        }
+    }
+
+    fn root(&self) -> Pos {
+        Pos {
+            st_i: 1,
+            range: 0..self.n,
+        }
+    }
+
+    fn recalc(&mut self, pos: &Pos, left: &Pos, right: &Pos) {
+        self.st[pos.st_i] = T::op(self.st[left.st_i], self.st[right.st_i]);
+    }
+
+    fn st_len(n: usize) -> usize {
+        let mut ret = 1;
+        while ret < n {
+            ret *= 2;
+        }
+        ret * 2
+    }
 }
 
 struct Pos {
@@ -57,89 +166,5 @@ impl Pos {
                 range: mid..end,
             },
         )
-    }
-}
-
-impl<T: Default + Clone> SegTree<T> {
-    pub fn new(a: &[T], f: CombineFn<T>) -> Self {
-        let n = a.len();
-        let mut ret = Self {
-            n,
-            st: def_vec(Self::st_len(n)),
-            f,
-        };
-        if n > 0 {
-            ret.build(a, &ret.root());
-        }
-        ret
-    }
-
-    pub fn get(&self, rng_bounds: impl RangeBounds<usize>) -> T {
-        let rng = unpack_range(rng_bounds);
-        assert_range(&rng, 0..self.n, false);
-        self.calc(&rng, &self.root())
-    }
-
-    pub fn set(&mut self, i: usize, val: T) {
-        assert!(i < self.n);
-        self.update(i, val, &self.root());
-    }
-
-    fn update(&mut self, i: usize, val: T, pos: &Pos) {
-        if pos.single_point().is_some() {
-            self.st[pos.st_i] = val;
-        } else {
-            let (ref left, ref right) = pos.split();
-            if left.contains(i) {
-                self.update(i, val, left);
-            } else {
-                self.update(i, val, right);
-            }
-            self.recalc(pos, left, right);
-        }
-    }
-
-    fn calc(&self, range: &Range<usize>, pos: &Pos) -> T {
-        if pos.is_inside(range) {
-            return self.st[pos.st_i].clone();
-        }
-        let (ref left, ref right) = pos.split();
-        if !right.intersects(range) {
-            self.calc(range, left)
-        } else if !left.intersects(range) {
-            self.calc(range, right)
-        } else {
-            (self.f)(&self.calc(range, left), &self.calc(range, right))
-        }
-    }
-
-    fn build(&mut self, a: &[T], pos: &Pos) {
-        if let Some(i) = pos.single_point() {
-            self.st[pos.st_i] = a[i].clone();
-        } else {
-            let (ref left, ref right) = pos.split();
-            self.build(a, left);
-            self.build(a, right);
-            self.recalc(pos, left, right);
-        }
-    }
-
-    fn root(&self) -> Pos {
-        Pos {
-            st_i: 1,
-            range: 0..self.n,
-        }
-    }
-
-    fn recalc(&mut self, pos: &Pos, left: &Pos, right: &Pos) {
-        self.st[pos.st_i] = (self.f)(&self.st[left.st_i], &self.st[right.st_i]);
-    }
-
-    fn st_len(n: usize) -> usize {
-        let mut ret = 1;
-        while ret < n {
-            ret *= 2;
-        }
-        ret * 2
     }
 }
--- src/ds/tests/seg_tree.rs
+++ src/ds/tests/seg_tree.rs
@@ -1,20 +1,31 @@
-use crate::ds::seg_tree::SegTree;
+use crate::ds::seg_tree::{SegTree, SegTreeMonoid};
+use crate::plat::classic::includes::IntoVecExt;
 
 #[test]
 pub fn seg_tree_sum() {
+    #[derive(Copy, Clone)]
+    struct SegTreeEl(i32);
+    impl SegTreeMonoid for SegTreeEl {
+        fn op(l: Self, r: Self) -> Self {
+            Self(l.0 + r.0)
+        }
+        fn e() -> Self {
+            Self(0)
+        }
+    }
     let a = vec![1, 10, 5, 6, -5, 4, 3, 13, 3, 4, 8, 7, 9, 10, 8, 9, 9, 1];
     let n = a.len();
     for start in 0..n {
         for end in start..=n {
             let mut cur = a[start..end].to_vec();
-            let mut st = SegTree::new(&cur, |x, y| *x + *y);
+            let mut st = SegTree::new(&cur.iter().map(|x| SegTreeEl(*x)).into_vec());
             for i in 0..cur.len() {
                 let v = (i + end - start) as i32;
                 cur[i] = v;
-                st.set(i, v);
+                st.set(i, SegTreeEl(v));
                 for l in 0..cur.len() {
                     for r in l..cur.len() {
-                        let actual = st.get(l..=r);
+                        let actual = st.get(l..=r).0;
                         let expected = cur[l..=r].iter().sum();
                         assert_eq!(actual, expected, "l = {l}, r = {r}, a = {cur:?}");
                     }
