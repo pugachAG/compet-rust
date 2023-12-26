42a4d1bf191c049792750bb26edd49fec7805011
Anton Puhach <pugachag@gmail.com>
2023-12-26 11:53:20 +0100

    Add combinations_count

--- src/math/combinatorics.rs
+++ src/math/combinatorics.rs
@@ -1,5 +1,24 @@
+use std::cmp::min;
+
 pub fn pair_count(n: usize) -> usize {
     n * n.saturating_sub(1) / 2
+}
+
+pub fn combinations_count(n: usize, k: usize) -> usize {
+    let k = min(n - k, k);
+    let mut ans = 1;
+    let mut nxt = 1;
+    for i in 0..k {
+        ans *= n - i;
+        while nxt <= k && ans % nxt == 0 {
+            ans /= nxt;
+            nxt += 1;
+        }
+    }
+    for v in nxt..=k {
+        ans /= v;
+    }
+    ans
 }
 
 pub fn factorial(n: usize) -> usize {
--- src/math/tests/combinatorics.rs
+++ src/math/tests/combinatorics.rs
@@ -1,8 +1,14 @@
-use crate::math::combinatorics::factorial;
+use crate::math::combinatorics::{factorial, combinations_count};
 
 #[test]
 fn factorial_basic() {
     assert_eq!(factorial(0), 1);
     assert_eq!(factorial(1), 1);
     assert_eq!(factorial(5), 120);
+}
+
+#[test]
+fn combinations_count_basic() {
+    assert_eq!(combinations_count(27, 9), 4686825);
+    assert_eq!(combinations_count(27, 18), 4686825);
 }
